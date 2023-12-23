29ccfa7bcacd95d28d9d5649ab67012a3a60bd86
Malav1313 <145552831+Malav1313@users.noreply.github.com>
2023-12-23 01:55:29 +0000

    code changes

--- Malav_Patel/2303031057065/P-6.c
+++ Malav_Patel/2303031057065/P-6.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+typedef struct Node {
+	int data;
+	struct Node* next;
+} Node;
+
+void printNthFromLast(Node* head, int N)
+{
+	int len = 0, i;
+	Node* temp = head;
+	while (temp != NULL) {
+		temp = temp->next;
+		len++;
+	}
+
+	if (len < N)
+		return;
+	temp = head;
+	for (i = 1; i < len - N + 1; i++)
+		temp = temp->next;
+	printf("%d", temp->data);
+	return;
+}
+void push(struct Node** head_ref, int new_data)
+{
+	Node* new_node = (Node*)malloc(sizeof(Node));
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
 int main()
 {
-    int i;
-    char word[50];
-    int len = 0;
-    printf("Type ");
-    scanf("%s", word);
-    for (i = 0; word[i] != '\0'; i++)
-    {
-        len++;
-    }
 
-    // int len2=strlen(word);
-    // printf("Length is %d",len2);
-    for (i = 0; i < len; i++)
-    {
-
-        printf("%c ", word[i]);
-        printf("2303031057065\n");
-    }
+	struct Node* head = NULL;
+	push(&head, 20);
+	push(&head, 4);
+	push(&head, 15);
+	push(&head, 35);
+	printNthFromLast(head, 4);
+	return 0;
 }
